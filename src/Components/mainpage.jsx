812eb39b5b154e20ca4ebf1d8a768f36b91ff909
MohammadBinAftab <48010758+MohammadBinAftab@users.noreply.github.com>
2024-08-15 18:36:12 +0530

    Update mainpage.jsx

--- src/Components/mainpage.jsx
+++ src/Components/mainpage.jsx
@@ -94,7 +94,7 @@ const styles = {
     borderRadius: '4px 0 0 4px',
     border: '1px solid #ccc',
     outline: 'none',
-    width: '300px'
+    width: '200px'
   },
   button: {
     padding: '10px 20px',
